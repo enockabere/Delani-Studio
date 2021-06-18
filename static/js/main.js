4cf02533aa9be4455c0e2346765bba125101536e
Enock Abere <maebaenock95@gmail.com>
2021-06-19 01:04:46 +0300

    add services section and styling

--- static/js/main.js
+++ static/js/main.js
@@ -4,3 +4,16 @@ $(function() {
      return false;
    });
  });
+
+// navigation bar
+window.onscroll =function () {stickTop()};
+var navbar = document.getElementById("navbar");
+var sticky = navbar.offsetTop;
+
+function stickTop(){
+  if (window.pageYOffset >=sticky){
+    navbar.classlist.add("sticky")
+  }else {
+    navbar.classlist.remove ("sticky")
+  }
+}
