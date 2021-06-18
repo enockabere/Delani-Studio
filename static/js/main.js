bd3d445101ec9e52a99f00f75d31a062ed10118b
Enock Abere <maebaenock95@gmail.com>
2021-06-19 01:36:13 +0300

    add jquery to toggle icons

--- static/js/main.js
+++ static/js/main.js
@@ -5,6 +5,39 @@ $(function() {
    });
  });
 
+
+ $(document).ready(function(){
+   $("#devImg").click(function(){
+     $("#devImg").slideDown('500').hide('500');
+     $("#dev").show('500');
+   });
+   $("#dev").click(function(){
+     $("#dev").slideUp('500');
+     $("#devImg").slideDown('500');
+   });
+ });
+
+ $(document).ready(function(){
+   $("#designImg").click(function(){
+     $("#designImg").slideDown('500').hide('500');
+     $("#design").show('500');
+   });
+   $("#design").click(function(){
+     $("#design").slideUp('500');
+     $("#designImg").slideDown('500');
+   });
+ });
+
+ $(document).ready(function(){
+   $("#productImg").click(function(){
+     $("#productImg").slideDown('500').hide('500');
+     $("#product").show('500');
+   });
+   $("#product").click(function(){
+     $("#product").slideUp('500');
+     $("#productImg").slideDown('500');
+   });
+ });
 // navigation bar
 window.onscroll =function () {stickTop()};
 var navbar = document.getElementById("navbar");
